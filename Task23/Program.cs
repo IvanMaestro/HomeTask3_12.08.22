4d5ea0742694d900c872473a4267e89d108e514b
Masterov_IN <masterov_in@mail.ru>
2022-08-12 14:37:47 +0300

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -4,3 +4,16 @@
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+double result = count;
+Console.WriteLine($"Таблица кубов чисел от 1 до {number}: ");
+while (count <= number)
+{
+    result = Math.Pow (count, 3);
+    Console.WriteLine($"{count} | {result}");
+    count++;
+}
