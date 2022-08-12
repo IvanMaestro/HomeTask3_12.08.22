1720a6c68094a8ed5d281dcc33af9a26173533a4
Masterov_IN <masterov_in@mail.ru>
2022-08-12 14:37:12 +0300

    Task19 done

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,19 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Для проверки является ли число палиндромом");
+Console.Write("Введите любое пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = number / 10000;
+int digit2 = number / 1000 % 10;
+int digit4 = number % 100 / 10;
+int digit5 = number % 10;
+
+if (number < 1000 || number > 99999) Console.WriteLine($"{number} - не пятизначное число!");
+else if (digit1 == digit5 && digit2 == digit4) Console.WriteLine("Да!");
+else
+{
+    Console.WriteLine("Нет!");
+}
